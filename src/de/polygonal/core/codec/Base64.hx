52aa180fc3da0c134fb181a0ba9beed05e123ea8
Michael Baczynski <michaelbaczynski@gmail.com>
2013-03-09 17:35:10 +0100

    io -> codec

--- src/de/polygonal/core/codec/Base64.hx
+++ src/de/polygonal/core/codec/Base64.hx
@@ -27,7 +27,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package de.polygonal.core.io;
+package de.polygonal.core.codec;
 
 import haxe.ds.Vector;
 import haxe.io.Bytes;
--- src/de/polygonal/core/codec/RLE.hx
+++ src/de/polygonal/core/codec/RLE.hx
@@ -27,7 +27,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package de.polygonal.core.io;
+package de.polygonal.core.codec;
 
 import de.polygonal.core.math.Limits;
 import haxe.io.Bytes;
@@ -157,7 +157,7 @@ class RLE
 	 */
 	public static inline function decodeString(encoded:Bytes):String
 	{
-		return decodeBytes(Bytes.ofString(encoded)).toString();
+		return decodeBytes(Bytes.ofString(encoded.toString())).toString();
 	}
 	
 	/**
