a26af53f651241a26eaae383c9f23e45582cbf36
Michael Baczynski <michaelbaczynski@gmail.com>
2013-11-17 14:25:15 +0100

    es add entity iterator helper class

---
+++ src/de/polygonal/core/es/EntityIterator.hx
@@ -0,0 +1,124 @@
+/*
+ *                            _/                                                    _/
+ *       _/_/_/      _/_/    _/  _/    _/    _/_/_/    _/_/    _/_/_/      _/_/_/  _/
+ *      _/    _/  _/    _/  _/  _/    _/  _/    _/  _/    _/  _/    _/  _/    _/  _/
+ *     _/    _/  _/    _/  _/  _/    _/  _/    _/  _/    _/  _/    _/  _/    _/  _/
+ *    _/_/_/      _/_/    _/    _/_/_/    _/_/_/    _/_/    _/    _/    _/_/_/  _/
+ *   _/                            _/        _/
+ *  _/                        _/_/      _/_/
+ *
+ * POLYGONAL - A HAXE LIBRARY FOR GAME DEVELOPERS
+ * Copyright (c) 2013 Michael Baczynski, http://www.polygonal.de
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package de.polygonal.core.es;
+
+class EntityIterator
+{
+	public static function descendants(e:Entity):Iterator<Entity>
+	{
+		var i = 0;
+		var s = e.size;
+		var walker = e.preorder;
+		return
+		{
+			hasNext: function()
+			{
+				return i < s;
+			},
+			next: function()
+			{
+				i++;
+				var t = walker;
+				walker = walker.preorder;
+				return t;
+			}
+		}
+	}
+	
+	public static function children(e:Entity):Iterator<Entity>
+	{
+		var walker = e.child;
+		return
+		{
+			hasNext: function()
+			{
+				return walker != null;
+			},
+			next: function()
+			{
+				var t = walker;
+				walker = walker.sibling;
+				return t;
+			}
+		}
+	}
+	
+	public static function ancestors(e:Entity):Iterator<Entity>
+	{
+		var walker = e.parent;
+		return
+		{
+			hasNext: function()
+			{
+				return walker != null;
+			},
+			next: function()
+			{
+				var t = walker;
+				walker = walker.parent;
+				return t;
+			}
+		}
+	}
+	
+	public static function siblings(e:Entity):Iterator<Entity>
+	{
+		if (e.parent == null)
+		{
+			return
+			{
+				hasNext: function() return false,
+				next: function() return null
+			}
+		}
+		
+		var walker = e.parent.child;
+		return
+		{
+			hasNext: function()
+			{
+				if (walker == e && walker.sibling == null) return false;
+				return walker != null;
+			},
+			next: function()
+			{
+				var t = walker;
+				if (t == e)
+				{
+					walker = walker.sibling;
+					t = walker;
+				}
+				if (walker != null)
+					walker = walker.sibling;
+				return t;
+			}
+		}
+	}
+}
---
+++ test/TestEntityIterator.hx
@@ -0,0 +1,113 @@
+package;
+
+import de.polygonal.core.es.Entity;
+import de.polygonal.core.es.EntitySystem;
+import haxe.unit.TestCase;
+
+using de.polygonal.core.es.EntityIterator;
+
+@:access(de.polygonal.core.es.Entity)
+@:access(de.polygonal.core.es.EntitySystem)
+class TestEntityIterator extends TestCase
+{
+	public function new()
+	{
+		super();
+		EntitySystem.init();
+	}
+	
+	public function testDescendants()
+	{
+		var a = new Entity('a');
+		var b = new Entity('b');
+		var c = new Entity('c');
+		
+		var result:Array<String> = [];
+		
+		for (e in a.descendants()) result.push(e.name);
+		assertEquals("", result.join(""));
+		result = [];
+		
+		a.add(b);
+		
+		for (e in a.descendants()) result.push(e.name);
+		assertEquals("b", result.join(""));
+		result = [];
+		
+		a.add(c);
+		
+		for (e in a.descendants()) result.push(e.name);
+		assertEquals("bc", result.join(""));
+	}
+	
+	public function testAncestors()
+	{
+		var a = new Entity('a');
+		var b = new Entity('b');
+		var c = new Entity('c');
+		
+		var result:Array<String> = [];
+		
+		for (e in a.ancestors()) result.push(e.name);
+		assertEquals("", result.join(""));
+		result = [];
+		
+		a.add(b);
+		
+		for (e in b.ancestors()) result.push(e.name);
+		assertEquals("a", result.join(""));
+		result = [];
+		
+		b.add(c);
+		
+		for (e in c.ancestors()) result.push(e.name);
+		assertEquals("ba", result.join(""));
+	}
+	
+	public function testSiblings()
+	{
+		var a = new Entity('a');
+		var b = new Entity('b');
+		var c = new Entity('c');
+		var d = new Entity('d');
+		
+		var result:Array<String> = [];
+		
+		for (e in a.siblings()) result.push(e.name);
+		assertEquals("", result.join(""));
+		result = [];
+		
+		a.add(b);
+		
+		for (e in b.siblings()) result.push(e.name);
+		assertEquals("", result.join(""));
+		result = [];
+		
+		a.add(c);
+		
+		for (e in b.siblings()) result.push(e.name);
+		assertEquals("c", result.join(""));
+		result = [];
+		
+		for (e in c.siblings()) result.push(e.name);
+		assertEquals("b", result.join(""));
+		result = [];
+		
+		a.add(d);
+		
+		for (e in d.siblings()) result.push(e.name);
+		assertEquals("bc", result.join(""));
+		result = [];
+		
+		for (e in d.siblings()) result.push(e.name);
+		assertEquals("bc", result.join(""));
+		result = [];
+		
+		for (e in c.siblings()) result.push(e.name);
+		assertEquals("bd", result.join(""));
+		result = [];
+		
+		for (e in b.siblings()) result.push(e.name);
+		assertEquals("cd", result.join(""));
+	}
+}
